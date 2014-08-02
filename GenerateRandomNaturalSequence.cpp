4452c6ef57f4dacb45a98e30c3b6908dbb04ee7d
David Flores <nonnumericdave@gmail.com>
2014-08-02 12:11:51 -0600

    + Allocate an array containing the specified range.
    -- daf

--- GenerateRandomNaturalSequence.cpp
+++ GenerateRandomNaturalSequence.cpp
@@ -5,4 +5,11 @@ void
 GenerateRandomNaturalSequence(size_t uStartOfRange, size_t uEndOfRange)
 {
 	assert( uStartOfRange <= uEndOfRange );
+
+	size_t* puRandomNaturalSequenceArray = new size_t[uEndOfRange - uStartOfRange];
+
+	for (size_t uIndex = uStartOfRange; uIndex <= uEndOfRange; uIndex++)
+		puRandomNaturalSequenceArray[uIndex - uStartOfRange] = uIndex;
+
+	delete [] puRandomNaturalSequenceArray;
 }
