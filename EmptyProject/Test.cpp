979227abcfad043dd75f26b60a7443ac31e71434
David Flores <nonnumericdave@gmail.com>
2014-08-07 23:52:27 -0600

    + Introduce EmptyProject in order to spawn off subprojects   quickly.
    -- daf

---
+++ EmptyProject/Test.cpp
@@ -0,0 +1,62 @@
+#include "Standard.h"
+
+// ~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~
+#define DefineUnitTest(pfnTest) { pfnTest, #pfnTest }
+
+// ~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~
+typedef bool(*PFN_Test)();
+
+// ~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~
+struct UnitTest
+{
+	PFN_Test pfnTest;
+	const char* szTest;
+};
+
+// ~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~
+static const UnitTest g_aUnitTest[] =
+{
+};
+
+// ~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~
+bool 
+TestDummy()
+{
+	return true;
+}
+
+// ~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~
+bool
+Test()
+{
+	::printf("\nRunning Unit Tests...\n");
+
+	bool bAllTestsHavePassed = TestDummy();
+	for (UnitTest unitTest : g_aUnitTest)
+	{
+		::printf(" %s ... ", unitTest.szTest);
+
+		bool bTestPassed = unitTest.pfnTest();
+
+		if ( bTestPassed )
+			::printf("passed\n");
+		else
+			::printf("failed\n");
+
+		bAllTestsHavePassed = bAllTestsHavePassed && bTestPassed;
+	}
+
+	if ( bAllTestsHavePassed )
+		::printf("Passed\n\n");
+	else
+		::printf("Failed\n\n");
+
+	return bAllTestsHavePassed;
+}
+
+// ~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~
+int
+main()
+{
+	return Test() ? 0 : 255;
+}
