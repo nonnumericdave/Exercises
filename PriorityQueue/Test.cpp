542b7b83efa80ad4a0fd8766d4a6f7471cdb8f6f
David Flores <nonnumericdave@gmail.com>
2014-08-13 22:17:13 -0600

    + Modify unit tests to allow testing of multiple priority   queue classes against the same data.
    -- daf

--- PriorityQueue/Test.cpp
+++ PriorityQueue/Test.cpp
@@ -17,6 +17,19 @@ struct UnitTest
 };
 
 // ~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~
+template<typename T, typename K>
+using PFN_PriorityQueueFactory = std::unique_ptr<PriorityQueue<T,K>>(*)();
+
+// ~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~
+PFN_PriorityQueueFactory<int, int> g_apfnPriorityQueueFactory[] =
+{
+	[] () -> std::unique_ptr<PriorityQueue<int, int>> 
+	{ 
+		return std::unique_ptr<PriorityQueue<int, int>>(new Heap<int, int>); 
+	},
+};
+
+// ~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~
 bool 
 TestSortedList()
 {
@@ -26,30 +39,34 @@ TestSortedList()
 
 	constexpr size_t kuValueCount = 1000;
 	std::vector<int> aValues(kuValueCount);
-	
 	for (size_t uIndex = 0; uIndex < kuValueCount; uIndex++)
 		aValues.push_back(uniformIntegerDistribution(mersenneTwisterGenerator));
 	
-	Heap<int, int> heap;
-	for (const int& iValue : aValues)
-		heap.InsertElementWithKey(iValue, iValue);
-
-	std::sort(aValues.begin(), aValues.end());
-
-	for (auto itValue = aValues.crbegin(); itValue != aValues.crend(); itValue++)
+	for (PFN_PriorityQueueFactory<int, int> pfnPriorityQueueFactory : g_apfnPriorityQueueFactory)
 	{
-		std::shared_ptr<PriorityQueue<int, int>::Element> pElement = heap.ExtractMaximumElement();
+		std::vector<int> aTempValues(aValues);
 
-		if ( pElement == nullptr )
-			return false;
+		std::unique_ptr<PriorityQueue<int, int>> pPriorityQueue(pfnPriorityQueueFactory());
+		for (const int& iValue : aTempValues)
+			pPriorityQueue->InsertElementWithKey(iValue, iValue);
 
-		if ( pElement->Value() != *itValue )
+		std::sort(aTempValues.begin(), aTempValues.end());
+
+		for (auto itValue = aTempValues.crbegin(); itValue != aTempValues.crend(); itValue++)
+		{
+			std::shared_ptr<PriorityQueue<int, int>::Element> pElement = pPriorityQueue->ExtractMaximumElement();
+
+			if ( pElement == nullptr )
+				return false;
+
+			if ( pElement->Value() != *itValue )
+				return false;
+		}
+
+		std::shared_ptr<PriorityQueue<int, int>::Element> pElement = pPriorityQueue->ExtractMaximumElement();
+		if ( pElement != nullptr )
 			return false;
 	}
-
-	std::shared_ptr<PriorityQueue<int, int>::Element> pElement = heap.ExtractMaximumElement();
-	if ( pElement != nullptr )
-		return false;
 
 	return true;
 }
@@ -61,31 +78,38 @@ TestIncreaseElementKey()
 	std::random_device randomDeviceGenerator;
 	std::mt19937 mersenneTwisterGenerator(randomDeviceGenerator());
 	std::uniform_int_distribution<int> uniformIntegerDistribution(1, 2500);
-
-	Heap<int, int> heap;
-
-	for (size_t uIndex = 0; uIndex < 4000; uIndex++)
-	{
-		const int iValue = uniformIntegerDistribution(mersenneTwisterGenerator);
-		heap.InsertElementWithKey(iValue, iValue);
-	}
-
-	std::shared_ptr<PriorityQueue<int, int>::Element> pInsertedElement =
-		heap.InsertElementWithKey(1, 1);
-
-	heap.IncreaseElementKey(pInsertedElement.get(), 2501);
-
-	for (size_t uIndex = 0; uIndex < 4500; uIndex++)
-	{
-		const int iValue = uniformIntegerDistribution(mersenneTwisterGenerator);
-		heap.InsertElementWithKey(iValue, iValue);
-	}
-
-	std::shared_ptr<PriorityQueue<int, int>::Element> pMaximumElement = 
-		heap.ExtractMaximumElement();
 	
-	if ( pInsertedElement != pMaximumElement )
-		return false;
+	constexpr size_t kuXValueCount = 4000;
+	std::vector<int> aXValues(kuXValueCount);
+	for (size_t uIndex = 0; uIndex < kuXValueCount; uIndex++)
+		aXValues.push_back(uniformIntegerDistribution(mersenneTwisterGenerator));
+	
+	constexpr size_t kuYValueCount = 4500;
+	std::vector<int> aYValues(kuYValueCount);
+	for (size_t uIndex = 0; uIndex < kuYValueCount; uIndex++)
+		aYValues.push_back(uniformIntegerDistribution(mersenneTwisterGenerator));
+
+	for (PFN_PriorityQueueFactory<int, int> pfnPriorityQueueFactory : g_apfnPriorityQueueFactory)
+	{
+		std::unique_ptr<PriorityQueue<int, int>> pPriorityQueue(pfnPriorityQueueFactory());
+
+		for (int iValue : aXValues)
+			pPriorityQueue->InsertElementWithKey(iValue, iValue);
+
+		std::shared_ptr<PriorityQueue<int, int>::Element> pInsertedElement =
+			pPriorityQueue->InsertElementWithKey(1, 1);
+
+		pPriorityQueue->IncreaseElementKey(pInsertedElement.get(), 2501);
+
+		for (int iValue : aYValues)
+			pPriorityQueue->InsertElementWithKey(iValue, iValue);
+
+		std::shared_ptr<PriorityQueue<int, int>::Element> pMaximumElement = 
+			pPriorityQueue->ExtractMaximumElement();
+		
+		if ( pInsertedElement != pMaximumElement )
+			return false;
+	}
 
 	return true;
 }
@@ -101,50 +125,50 @@ TestMerge()
 	constexpr size_t kuValueCount = 1000;
 
 	std::vector<int> aXValues(kuValueCount);
-	
 	for (size_t uIndex = 0; uIndex < kuValueCount; uIndex++)
 		aXValues.push_back(uniformIntegerDistribution(mersenneTwisterGenerator));
 	
-	Heap<int, int> heapX;
-	for (const int& iValue : aXValues)
-		heapX.InsertElementWithKey(iValue, iValue);
-
-	std::sort(aXValues.begin(), aXValues.end());
-
 	std::vector<int> aYValues(kuValueCount);
-	
 	for (size_t uIndex = 0; uIndex < kuValueCount; uIndex++)
 		aYValues.push_back(uniformIntegerDistribution(mersenneTwisterGenerator));
 	
-	Heap<int, int> heapY;
-	for (const int& iValue : aYValues)
-		heapY.InsertElementWithKey(iValue, iValue);
-
-	std::sort(aYValues.begin(), aYValues.end());
-
-	std::vector<int> aValues;
-	std::merge(aXValues.begin(), aXValues.end(), aYValues.begin(), aYValues.end(), std::back_inserter(aValues));
-
-	heapX.MergePriorityQueue(&heapY);
-
-	std::shared_ptr<PriorityQueue<int, int>::Element> pElement = heapY.ExtractMaximumElement();
-	if ( pElement != nullptr )
-		return false;
-
-	for (auto itValue = aValues.crbegin(); itValue != aValues.crend(); itValue++)
+	for (PFN_PriorityQueueFactory<int, int> pfnPriorityQueueFactory : g_apfnPriorityQueueFactory)
 	{
-		std::shared_ptr<PriorityQueue<int, int>::Element> pElement = heapX.ExtractMaximumElement();
+		std::unique_ptr<PriorityQueue<int, int>> pXPriorityQueue(pfnPriorityQueueFactory());
+		for (int iValue : aXValues)
+			pXPriorityQueue->InsertElementWithKey(iValue, iValue);
 
-		if ( pElement == nullptr )
+		std::unique_ptr<PriorityQueue<int, int>> pYPriorityQueue(pfnPriorityQueueFactory());
+		for (int iValue : aYValues)
+			pYPriorityQueue->InsertElementWithKey(iValue, iValue);
+
+		std::vector<int> aValues(aXValues);
+		aValues.reserve(aXValues.size() + aYValues.size());
+		aValues.insert(aValues.end(), aYValues.begin(), aYValues.end());
+
+		std::sort(aValues.begin(), aValues.end());
+
+		pXPriorityQueue->MergePriorityQueue(pYPriorityQueue.get());
+
+		std::shared_ptr<PriorityQueue<int, int>::Element> pElement = pYPriorityQueue->ExtractMaximumElement();
+		if ( pElement != nullptr )
 			return false;
 
-		if ( pElement->Value() != *itValue )
+		for (auto itValue = aValues.crbegin(); itValue != aValues.crend(); itValue++)
+		{
+			std::shared_ptr<PriorityQueue<int, int>::Element> pElement = pXPriorityQueue->ExtractMaximumElement();
+
+			if ( pElement == nullptr )
+				return false;
+
+			if ( pElement->Value() != *itValue )
+				return false;
+		}
+
+		pElement = pXPriorityQueue->ExtractMaximumElement();
+		if ( pElement != nullptr )
 			return false;
 	}
-
-	pElement = heapX.ExtractMaximumElement();
-	if ( pElement != nullptr )
-		return false;
 
 	return true;
 }
